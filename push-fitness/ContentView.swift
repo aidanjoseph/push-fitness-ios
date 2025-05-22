c3ea14ed0fe85fcfd63c14dcefcfd3f90d74b268
Aidan Joseph <aj626@cornell.edu>
2025-05-22 14:56:22 -0400

    basic ssetup

---
+++ push-fitness/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  push-fitness
+//
+//  Created by Aidan Joseph on 5/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ push-fitness/push_fitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  push_fitnessApp.swift
+//  push-fitness
+//
+//  Created by Aidan Joseph on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct push_fitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
